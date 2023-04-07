4b0c73d4730dbb8e081b3d61fdbdd07cbda16caa
csengekulker <k71608883429@ktch.hu>
2023-04-07 16:17:53 +0200

    reset inputs solved w clearTextOnFocus prop

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 import { useState } from 'react';
-import { TextInput,  Text, View } from 'react-native';
+import { TextInput, Text, View } from 'react-native';
 import { TouchableHighlight } from 'react-native-web';
 import { styles } from './App.style';
 
 export default function App() {
+  function calcPercentage(first, second) {
+    let cent = first + second
+
+    let firstPercent = 100 * first / cent
+    let secondPercent = 100 * second / cent
+
+    console.log(cent)
+
+    setFirstPercent(firstPercent)
+    setSecondPercent(secondPercent)
+
+  }
+
   function submit() {
-    console.log('submit');
-    // calc percentages
+    let result = calcPercentage(Number(first), Number(second))
     // reveal text element with result
   }
 
-  function reset() {
-    console.log('reset');
-    setFirst(''); setSecond('')
-  }
-
-  const [first, setFirst] = useState('')
-  const [second, setSecond] = useState('')
+  const [first, setFirst] = useState(0)
+  const [second, setSecond] = useState(0)
+  const [firstPercent, setFirstPercent] = useState(0)
+  const [secondPercent, setSecondPercent] = useState(0)
 
   return (
-        <View style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Almaosztály</Text>
 
       <View
@@ -27,22 +36,24 @@ export default function App() {
 
         <Text>Első osztály:</Text>
         <TextInput
-          onChangeText={(first) => setFirst(first)}
+          clearTextOnFocus
+          onChangeText={(first) => setFirst(Number(first))}
           style={styles.input} />
 
         <Text>Másodosztály:</Text>
         <TextInput
-          onChangeText={(second) => setSecond(second)}
-          style={styles.input}/>
+          clearTextOnFocus
+          onChangeText={(second) => setSecond(Number(second))}
+          style={styles.input} />
 
-          <TouchableHighlight onPress={submit} value='Submit' style={[styles.submit, styles.button]}>
-            <Text>Submit</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={reset} value='Again' style={[ styles.reset, styles.button]}>
-            <Text>Again</Text>
-          </TouchableHighlight>
+        <TouchableHighlight onPress={submit} value='Submit' style={[styles.submit, styles.button]}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
 
-        <Text>{first}, {second}</Text>
+        <Text>Első osztály {firstPercent} %</Text>
+
+        <Text>Másodosztály {secondPercent} %</Text>
+
       </View>
     </View>
   );
